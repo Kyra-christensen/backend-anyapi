4acfccbc44752cfe525e04f51ee7bde536378f38
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-15 19:54:09 -0700

    created delete test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,52 +12,60 @@ describe('catologue routes', () => {
   afterAll(() => {
     pool.end();
   });
-});
 
-it('should create a cat', async () => {
-  const res = await request(app)
-    .post('/api/v1/cats')
-    .send({ name: 'Thor', age: 2, favoriteToy: 'Socks' });
-    
-  expect(res.body).toEqual({
-    id: expect.any(String),
-    name: 'Thor',
-    age: 2,
-    favoriteToy: 'Socks',
-  });
-});
 
-it('should be able to list a cat by id', async () => {
-  const cat = await Cat.insert({ name: 'Thor', age: 2, favoriteToy: 'Sock' });
-  const res = await request(app).get(`/api/v1/cats/${cat.id}`);
-  expect(res.body).toEqual(cat);
-});
-
-it('should be able to list cats', async () => {
-  await Cat.insert({ name: 'Thor', age: 2, favoriteToy: 'Socks' });
-  const res = await request(app).get('/api/v1/cats');
-  expect(res.body).toEqual([
-    {
+  it('should create a cat', async () => {
+    const res = await request(app)
+      .post('/api/v1/cats')
+      .send({ name: 'Thor', age: 2, favoriteToy: 'Socks' });
+      
+    expect(res.body).toEqual({
       id: expect.any(String),
       name: 'Thor',
       age: 2,
-      favoriteToy: 'Socks'
-    }
-  ]);
-});
+      favoriteToy: 'Socks',
+    });
+  });
 
-it('should be able to update a cat', async () => {
-  const cat = await Cat.insert({ name: 'Thor', age: 2, favoriteToy: 'Socks' });
-  const res = await request(app)
-    .patch(`/api/v1/cats/${cat.id}`)
-    .send({ name: 'Liam', age: 2, favoriteToy: 'Catnip' });
+  it('should be able to list a cat by id', async () => {
+    const cat = await Cat.insert({ name: 'Thor', age: 2, favoriteToy: 'Sock' });
+    const res = await request(app).get(`/api/v1/cats/${cat.id}`);
+    expect(res.body).toEqual(cat);
+  });
 
-  const expected = {
-    id: expect.any(String),
-    name: 'Liam',
-    age: 2,
-    favoriteToy: 'Catnip',
-  };
-  expect(res.body).toEqual(expected);
-  expect(await Cat.getById(cat.id)).toEqual(expected);
+  it('should be able to list cats', async () => {
+    await Cat.insert({ name: 'Thor', age: 2, favoriteToy: 'Socks' });
+    const res = await request(app).get('/api/v1/cats');
+    expect(res.body).toEqual([
+      {
+        id: expect.any(String),
+        name: 'Thor',
+        age: 2,
+        favoriteToy: 'Socks'
+      }
+    ]);
+  });
+
+  it('should be able to update a cat', async () => {
+    const cat = await Cat.insert({ name: 'Thor', age: 2, favoriteToy: 'Socks' });
+    const res = await request(app)
+      .patch(`/api/v1/cats/${cat.id}`)
+      .send({ name: 'Liam', age: 2, favoriteToy: 'Catnip' });
+
+    const expected = {
+      id: expect.any(String),
+      name: 'Liam',
+      age: 2,
+      favoriteToy: 'Catnip',
+    };
+    expect(res.body).toEqual(expected);
+    expect(await Cat.getById(cat.id)).toEqual(expected);
+  });
+
+  it('should be able to delete a cat', async () => {
+    const cat = await Cat.insert({ name: 'Thor', age: 2, favoriteToy: 'Socks' });
+    const res = await request(app).delete(`/api/v1/cats/${cat.id}`);
+    expect(res.body).toEqual(cat);
+    expect(await Cat.getById(cat.id)).toBeNull();
+  });
 });
