f82a102a48820fbf9bd6512dfb2e77638fafea90
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-15 19:16:28 -0700

    created insert test and insert .post in cats.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Cat = require('../lib/models/Cat');
 
 describe('catologue routes', () => {
   beforeEach(() => {
@@ -10,5 +11,18 @@ describe('catologue routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+});
+
+it('should create a cat', async () => {
+  const res = await request(app)
+    .post('/api/v1/cats')
+    .send({ name: 'Thor', age: 2, favoriteToy: 'Socks' });
+    
+  expect(res.body).toEqual({
+    id: expect.any(String),
+    name: 'Thor',
+    age: 2,
+    favoriteToy: 'Socks',
   });
 });
